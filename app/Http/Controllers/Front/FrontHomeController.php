6cb19c80f1c36f453ffc4dcd8f992de64dd6ad4e
Elvin <84231975+ElvinEsedullayev@users.noreply.github.com>
2022-07-12 20:48:29 +0400

    product discount show

--- app/Http/Controllers/Front/FrontHomeController.php
+++ app/Http/Controllers/Front/FrontHomeController.php
@@ -13,10 +13,11 @@ class FrontHomeController extends Controller
         $sliderBanners = Banner::where('type','Slider')->where('status',1)->get()->toArray();
         $fixBanners = Banner::where('type','Fix')->where('status',1)->get()->toArray();
         //dd($fixBanners);
-        $newProducts = Product::orderBy('id','Desc')->where('status',1)->limit(4)->get()->toArray();
+        $newProducts = Product::orderBy('id','Desc')->where('status',1)->limit(8)->get()->toArray();
         //dd($newProducts);
         $bestSellers = Product::where(['is_bestseller' => 'Yes','status' => 1])->inRandomOrder()->get()->toArray();
         //dd($bestSellers);
-        return view('front.index')->with(compact('sliderBanners','fixBanners','newProducts','bestSellers'));
+        $discountedProducts = Product::where('product_discount','>',0)->where('status',1)->inRandomOrder()->get()->toArray();
+        return view('front.index')->with(compact('sliderBanners','fixBanners','newProducts','bestSellers','discountedProducts'));
     }
 }
